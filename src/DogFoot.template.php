b6278cb9cfb8329fd39910e83a8181a46e46ae06
thoratica <me@tica.fun>
2022-03-14 23:53:08 +0900

    wa

--- src/DogFoot.template.php
+++ src/DogFoot.template.php
@@ -31,8 +31,8 @@ class DogFootTemplate extends BaseTemplate
             if ($this->getSkin()->getUser()->isRegistered()) {
             ?>
               <div class="profile group">
-                <button class="px-3.5 py-2 transition-colors duration-300 rounded-lg outline-none focus:bg-gray-100 hover:bg-gray-100">
-                  <i class="fa-solid fa-user text-gray-900"></i>
+                <button class="px-3.5 py-2 transition-colors duration-300 ease-in-out rounded-xl outline-none bg-gray-100 focus:bg-gray-200 hover:bg-gray-200">
+                  <i class="fa-solid fa-user text-gray-500"></i>
                 </button>
                 <div class="fixed h-0">
                   <ul class="relative bg-gray-50 p-1.5 border border-gray-50 shadow-lg rounded-lg right-[calc(100%_-_42px)] opacity-0 invisible transform -translate-y-4 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:translate-x-1 group-hover:translate-y-0 group-focus-within:opacity-100 group-focus-within:visible group-focus-within:translate-x-1 group-focus-within:translate-y-0">
@@ -81,9 +81,41 @@ class DogFootTemplate extends BaseTemplate
         <section class="mx-auto px-3 xs:px-6 flex max-w-7xl w-full">
           <article class="py-6 w-full">
             <div class="flex flex-col border-0 mb-2.5">
-              <h1 class="text-gray-900 outline-none focus:filter focus:brightness-95 transition-all font-semibold text-3xl xs:text-4xl m-0 p-0 border-0">
-                <?php $this->html('title'); ?>
-              </h1>
+              <div class="flex items-center">
+                <h1 class="text-gray-900 outline-none focus:filter focus:brightness-95 transition-all font-semibold text-3xl xs:text-4xl m-0 p-0 border-0">
+                  <?php $this->html('title'); ?>
+                </h1>
+                <div class="ml-auto flex gap-x-1.5">
+                  <a href="<?php echo $this->data['content_actions']['ve-edit']['href'] ?? '#' ?>" class="bg-gray-100 hover:bg-gray-200 outline-none transition-colors duration-300 ease-in-out rounded-xl px-[11px] py-2" title="시각 편집 모드">
+                    <i class="fa-solid fa-eye text-gray-500"></i>
+                  </a>
+                  <a href="<?php echo $this->data['content_actions']['edit']['href'] ?? '#' ?>" class="bg-gray-100 hover:bg-gray-200 outline-none transition-colors duration-300 ease-in-out rounded-xl px-3 py-2" title="원본 편집 모드">
+                    <i class="fa-solid fa-pen text-gray-500"></i>
+                  </a>
+
+                  <div class="profile group">
+                    <button class="bg-gray-100 hover:bg-gray-200 outline-none transition-colors duration-300 ease-in-out rounded-xl px-[15px] py-2" title="더보기">
+                      <i class="fa-solid fa-caret-down text-gray-500"></i>
+                    </button>
+                    <div class="fixed h-0">
+                      <ul class="relative bg-gray-50 p-1.5 border border-gray-50 shadow-lg rounded-lg right-[calc(100%_-_42px)] opacity-0 invisible transform -translate-y-4 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:visible group-hover:translate-x-1 group-hover:translate-y-0 group-focus-within:opacity-100 group-focus-within:visible group-focus-within:translate-x-1 group-focus-within:translate-y-0">
+                        <li href="<?php echo $this->data['content_actions']['history']['href'] ?? '#' ?>" class="text-gray-900 hover:bg-gray-100 outline-none transition-colors duration-300 ease-in-out rounded-xl px-3 py-2" title="역사">
+                          <i class="fa-solid fa-clock-rotate-left text-gray-500"></i>
+                        </li>
+                        <li href="<?php echo $this->data['content_actions']['move']['href'] ?? '#' ?>" class="text-gray-900 hover:bg-gray-100 outline-none transition-colors duration-300 ease-in-out rounded-xl px-3 py-2" title="이동">
+                          <i class="fa-solid fa-angles-right text-gray-500"></i>
+                        </li>
+                        <li href="<?php echo $this->data['content_actions']['delete']['href'] ?? '#' ?>" class="text-gray-900 hover:bg-gray-100 outline-none transition-colors duration-300 ease-in-out rounded-xl px-3 py-2" title="삭제">
+                          <i class="fa-solid fa-trash text-gray-500"></i>
+                        </li>
+                        <li href="<?php echo $this->data['content_actions']['protect']['href'] ?? '#' ?>" class="text-gray-900 hover:bg-gray-100 outline-none transition-colors duration-300 ease-in-out rounded-xl px-3 py-2" title="보호">
+                          <i class="fa-solid fa-lock text-gray-500"></i>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <?php $this->html('catlinks'); ?>
             </div>
             <section id="post">

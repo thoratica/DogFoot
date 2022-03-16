984660840f775bc956092b6dc66fefda427b46dc
thoratica <me@tica.fun>
2022-03-16 11:01:09 +0900

    tailwindcss 제거 작업 후반

--- src/DogFoot.template.php
+++ src/DogFoot.template.php
@@ -16,6 +16,9 @@ class DogFootTemplate extends BaseTemplate
     $this->html("headelement");
 ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <div class="df-screen">
